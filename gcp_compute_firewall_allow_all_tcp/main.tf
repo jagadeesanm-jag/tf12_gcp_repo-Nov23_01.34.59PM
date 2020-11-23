621a14bcd6f9addba557da76c3423dcc0e1eec7b
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-23 13:35:44 +0530

    Modified files:[main.tf]

--- gcp_compute_firewall_allow_all_tcp/main.tf
+++ gcp_compute_firewall_allow_all_tcp/main.tf
@@ -49,3 +49,5 @@ resource "google_compute_firewall" "default" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2020-11-23 13:35:44.424620]
